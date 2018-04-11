458779194fb6573b574b50b63be52c65f1f08a21
tobi.dara <tobi_daramola@yahoo.com>
2018-04-11 17:04:20 +0100

    feature(server):configure server

---
+++ server/expressApp.js
@@ -0,0 +1,20 @@
+import { renderFile } from 'ejs';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import path from 'path';
+import expressStaticGzip from 'express-static-gzip';
+import routes from './routes';
+
+export default (expressApp) => {
+  expressApp.use(logger('dev'));
+  expressApp.use(bodyParser.json());
+  expressApp.use(bodyParser.urlencoded({
+    extended: false
+  }));
+
+  expressApp.use(expressStaticGzip(path.join(__dirname, '../client/build')));
+  expressApp.set('views', path.join(__dirname, '../client/build'));
+  expressApp.engine('.html', renderFile);
+
+  expressApp.use(routes);
+};

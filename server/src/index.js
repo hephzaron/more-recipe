cfd3474c0902b3bc7a43a17975cbb90042048be9
tobi.dara <tobi_daramola@yahoo.com>
2018-05-10 16:34:10 +0100

    trying to fix port error on heroku

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,7 @@ const io = socketIO(server);
 app.set('port', port);
 sockets(io);
 
-server.listen(port, () => {
+server.listen(port, '0.0.0.0', () => {
   console.log(`Server listening on port ${port}`);
 });
 

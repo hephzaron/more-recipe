4d0fe1751aeab2023aad0669760c63a1ff3dcb82
tobi.dara <tobi_daramola@yahoo.com>
2018-06-01 17:53:10 +0100

    change baseUrl

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -265,7 +265,7 @@ class UserController {
                 message: 'Your credentials could not be updated please try again'
               });
             }
-            const baseURL = 'https://more-recipe.herokuapp.com/api/v1';
+            const baseURL = 'https://waw-recipe.herokuapp.com/api/v1';
             const resetUrl = `${baseURL}/auth/reset_password?token=${resetPasswordToken}`;
             const { username } = updatedUser;
             const mailer = new Mailer();

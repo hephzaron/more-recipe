604eba0186505c871ee763d4b7c4c6147c706379
tobi.dara <tobi_daramola@yahoo.com>
2018-06-22 09:43:48 +0100

    chore(db):add db-url to env var

--- server/config/db.js
+++ server/config/db.js
@@ -15,7 +15,8 @@ const {
   DEVELOPMENT_DB_HOST,
   DEVELOPMENT_DB_PORT,
   DEVELOPMENT_DB_PASSWORD,
-  DEVELOPMENT_DB_USERNAME
+  DEVELOPMENT_DB_USERNAME,
+  DATABASE_URL
 } = process.env;
 
 module.exports = {
@@ -36,11 +37,6 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    username: PRODUCTION_DB_USERNAME,
-    password: PRODUCTION_DB_PASSWORD,
-    database: PRODUCTION_DB_NAME,
-    host: PRODUCTION_DB_HOST,
-    port: PRODUCTION_DB_PORT,
-    dialect: 'postgres'
+    use_env_variable: DATABASE_URL
   }
 };

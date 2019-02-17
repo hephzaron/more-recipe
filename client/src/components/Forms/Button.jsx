7a6acfe193296a92396f011beba4e9b861fbb0d8
tobi.dara <tobi_daramola@yahoo.com>
2019-02-17 17:39:22 +0100

    feature(forms): create button component

---
+++ client/src/components/Forms/Button.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+/**
+ * @description abstracts button element
+ * @param {object} props
+ * @return {void}
+ */
+
+const Button = (props) => (
+  <button
+    className = {classnames('btn',
+      props.btnClass,
+      props.iconClass ? `fa ${props.iconClass}` : '')}
+     type = {props.type}
+     disabled = {props.disabled}
+     onClick = {props.onClick}>
+      {props.name}
+  </button>);
+
+Button.propTypes = {
+    type: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    disabled: PropTypes.bool,
+    btnClass: PropTypes.string,
+    onClick: PropTypes.func,
+    iconClass: PropTypes.string
+};
+
+Button.defaultProps = {
+    type: 'submit',
+    disabled: true,
+    btnClass: 'btn-default'
+};
+
+export default Button;

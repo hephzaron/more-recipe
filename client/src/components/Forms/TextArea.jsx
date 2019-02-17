046e7cec5b7a319687e75ea5a07c83d4f580e364
tobi.dara <tobi_daramola@yahoo.com>
2019-02-17 17:53:06 +0100

    feature(forms): create the text area component

---
+++ client/src/components/Forms/TextArea.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * @description abstracts input field for text area
+ * @param {object} props
+ * @returns {JSX} JSX Component
+ */
+
+const TextArea = (props) => (
+		<textarea
+			className="form-control"
+			name={props.name}
+			placeholder={props.placeholder}
+			value={props.value}
+      onChange={props.onChange}
+      rows ={props.rows.toString()}/>
+);
+
+TextArea.propTypes = {
+	placeholder: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  onChange: PropTypes.fuunc,
+  rows: PropTypes.number.isRequired
+};
+
+TextArea.defaultProps = {
+  noRows: 5
+};
+
+export default TextArea;

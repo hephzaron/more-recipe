ce78e29e4032da25cc5046cef2a76f9c7825d8ac
Daramola Tobi <tobi_daramola@yahoo.com>
2022-08-23 00:27:32 +0100

    feat: Enable an authenticated user to logout of the application

--- client/src/components/general/Header.jsx
+++ client/src/components/general/Header.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faSearch, faBell } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHome, faSearch, faBell, faCaretUp
+} from "@fortawesome/free-solid-svg-icons";
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { logoutUser } from '../../actions/authUserActions';
 
 /**
  * @class Header
@@ -21,34 +24,48 @@ class Header extends Component {
     this.state = {
       values: {
         searchTerm: "",
-      }
+      },
+      showProfile: false
     };
-    this.toggleNav = this.toggleNav.bind(this);
+    this.logOut = this.logOut.bind(this);
   }
 
 /**
- * @description Creates an instance of Header class
+ * @description Navigates to the required page
  * @param { string } uri
  * @return {void}
  */
   navTo = (uri) => {
     window.location.href = window.location.origin + uri;
-    const aTag = document.getElementsByTagName('a');
-    console.log(aTag);
   }
 
-  submitSearchForm = async (event) => {
+  submitSearchForm = (event) => {
     event.preventDefault();
-    console.log(this.state.values);
   }
 
   inputChangeHandler = (event) => {
     this.setState({ values: { [event.target.name]: event.target.value } });
   }
 
+  toggleProfileList = () => {
+    const profileDropdown = document.getElementById('profile-dropdown');
+    profileDropdown.style.display = profileDropdown.style.display === "block" ? "" : "block";
+  }
+
   toggleNav = () => {
     const topNav = document.getElementById('topnav');
     topNav.style.display = topNav.style.display === "block" ? "" : "block";
+  }
+
+  /**
+   * @memberof Header
+   * @method logOut
+   * @param {null}-void
+   * @returns {null} void
+   */
+  logOut() {
+    this.props.dispatch(logoutUser());
+    this.navTo('/');
   }
 
   /**
@@ -63,50 +80,84 @@ class Header extends Component {
           <span id="toggle-nav" onClick= {() => this.toggleNav()}>
             <i className="fa fa-bars"/>
           </span>
-        <div className="topnav" id="topnav">
-          <a className={`${uri === '/' ? 'active' : ''}`} onClick={() => {
-            this.navTo('/');
-            }}><FontAwesomeIcon icon={faHome}/></a>
-          <a className={`${uri === '/about-us' ? 'active' : ''}`} onClick={() => {
-            this.navTo('/about-us');
-            }}>About Us</a>
-          <div className="search-container">
-            <form className="search-item" onSubmit={this.submitSearchForm}>
-              <FontAwesomeIcon className="fa-search" icon={faSearch}/>
-              <input
-                type="text"
-                name="searchTerm"
-                value={values.searchTerm}
-                onChange={this.inputChangeHandler}/>
-            </form>
+          <div className="topnav" id="topnav">
+            <a className={`${uri === '/' ? 'active' : ''}`} onClick={() => {
+              this.navTo('/');
+              }}><FontAwesomeIcon icon={faHome}/></a>
+            <a className={`${uri === '/about-us' ? 'active' : ''}`} onClick={() => {
+              this.navTo('/about-us');
+              }}>About Us</a>
+            <div className="search-container">
+              <form className="search-item" onSubmit={this.submitSearchForm}>
+                <FontAwesomeIcon className="fa-search" icon={faSearch}/>
+                <input
+                  type="text"
+                  name="searchTerm"
+                  value={values.searchTerm}
+                  onChange={this.inputChangeHandler}/>
+              </form>
+            </div>
+            {
+              !this.props.isAuthenticated &&
+              <div className="center-nav">
+                <button onClick={() => {
+                  this.navTo('/register');
+                  }} className="user-auth-join-us">JOIN US </button>
+                <button onClick={() => {
+                  this.navTo('/login');
+                  }} className="user-auth-login">LOGIN </button>
+              </div>
+            }
+            {
+              this.props.isAuthenticated &&
+              <ul className="auth-header">
+                <li><a>My Favorites</a></li>
+                <li><a>Create Recipe</a></li>
+                <li><a id="user-notification"><FontAwesomeIcon icon={faBell}/>
+                  <span className="no-of-notification"/></a>
+                </li>
+                <li>
+                  <a className="user-image" id="user-profile">
+                  <img
+                    src="https://www.mensjournal.com/wp-content/uploads/2018/10/rambo-main-3.jpg?quality=86&strip=all"
+                    alt="Recipe"
+                    className="profile_img"
+                    onClick={() => this.toggleProfileList()}/>
+                  <span className="hidden-header"
+                    style={{ marginLeft: "20px", position: "relative", top: "10px" }}>Daramola Oluwatobi</span>
+                  </a>
+                </li>
+                <ul className="hidden-header">
+                  <li><a>Change Password</a></li>
+                  <hr/>
+                  <li><a onClick={this.logOut}>Logout</a></li>
+                </ul>
+              </ul>
+            }
           </div>
           {
-            !this.props.isAuthenticated &&
-            <div className="center-nav">
-              <button onClick={() => {
-                this.navTo('/register');
-                }} className="user-auth-join-us">JOIN US </button>
-              <button onClick={() => {
-                this.navTo('/login');
-                }} className="user-auth-login">LOGIN </button>
+            this.props.isAuthenticated &&
+            <div className="profile-dropdown" id="profile-dropdown">
+              <FontAwesomeIcon className="profile-dropdown-caret" icon={faCaretUp}/>
+              <ul id="profile-menu">
+                <li>
+                  <img src="https://www.mensjournal.com/wp-content/uploads/2018/10/rambo-main-3.jpg?quality=86&strip=all"/>
+                  <a>Daramola Oluwatobi Daramola Oluwatobi</a>
+                </li>
+                <li>
+                  <a>Edit Profile</a>
+                </li>
+                <li>
+                  <a>Change Password</a>
+                </li>
+                <hr/>
+                <li>
+                  <a style={{ position: "relative", bottom: "-10px" }}
+                  onClick = {this.logOut}>Logout</a>
+                </li>
+              </ul>
             </div>
           }
-          {
-            this.props.isAuthenticated &&
-            <ul className="auth-header">
-              <li><a>My Favorites</a></li>
-              <li><a>Create Recipe</a></li>
-              <li><a id="user-notification"><FontAwesomeIcon icon={faBell}/>
-                <span className="no-of-notification"/></a>
-              </li>
-              <li>
-                <a className="user-image" id="user-profile">
-                <img src="https://www.mensjournal.com/wp-content/uploads/2018/10/rambo-main-3.jpg?quality=86&strip=all" alt="Recipe" className="profile_img" />
-                </a>
-              </li>
-            </ul>
-          }
-        </div>
         </div>
       );
     }
@@ -114,7 +165,8 @@ class Header extends Component {
 
 Header.propTypes = {
   isAuthenticated: PropTypes.bool.isRequired,
-  user: PropTypes.object
+  user: PropTypes.object,
+  dispatch: PropTypes.func
 };
 
 const mapStateToProps = (state) => ({

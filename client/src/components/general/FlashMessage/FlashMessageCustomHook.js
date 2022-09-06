0e480e52d55a45d6b3ea66eb1ec3d90ceb2d9fee
Daramola Tobi <tobi_daramola@yahoo.com>
2022-09-06 13:03:18 +0100

    feat: Add timeout and fix the position of flash message component

--- client/src/components/general/FlashMessage/FlashMessageCustomHook.js
+++ client/src/components/general/FlashMessage/FlashMessageCustomHook.js
@@ -1,10 +1,18 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 const useFlashMessage = (callback) => {
     const dispatch = useDispatch();
     const message = useSelector((state) => state.flashMessageReducer.message);
     const type = useSelector((state) => state.flashMessageReducer.type);
+
     const hideFlashMessage = () => dispatch(callback());
+
+    useEffect(() => {
+        const timer = setTimeout(() => hideFlashMessage(), 7000);
+        return () => clearTimeout(timer);
+    }, []);
+
     return {
         message, type, hideFlashMessage
     };

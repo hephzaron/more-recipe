62873812d808b952eeeb9b02aab30f662064c8f7
Daramola Tobi <tobi_daramola@yahoo.com>
2022-09-07 12:51:45 +0100

    refactor: Change React base app from a class component to a functional component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,4 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React from 'react';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import './assets/css/header.css';
 import './assets/css/homepage.css';
@@ -16,45 +14,29 @@ import Header from './components/general/Header';
 
 /**
  * React App
- * @class App
+ * @function App
  * @param {null} void
  */
-class App extends Component {
- /**
-   * Renders HomePage Component
-   * @method render
-   * @memberof App
-   * @param {null} void
-   * @returns { JSX }  JSX component
-   */
-  render = () => (
-      <Router>
-        <div>
-          <Header/>
-          <hr/>
-          <Routes>
-            <Route path="/">
-              <Route path="" element={<HomePage/>}/>
-              <Route path="register" element={<SignupForm/>}/>
-              <Route path="login" element={<LoginForm/>}/>
-              <Route path="*"
-                    element={
-                    <div style={{ padding: "20rem" }}>
-                      <p>There's nothing here!</p>
-                    </div>}/>
-            </Route>
-          </Routes>
-        </div>
-      </Router>
-    );
-}
 
-App.propTypes = {
-  isAuthenticated: PropTypes.bool.isRequired,
-};
+const App = () => (
+    <Router>
+      <div>
+        <Header/>
+        <hr/>
+        <Routes>
+          <Route path="/">
+            <Route path="" element={<HomePage/>}/>
+            <Route path="register" element={<SignupForm/>}/>
+            <Route path="login" element={<LoginForm/>}/>
+            <Route path="*"
+                  element={
+                  <div style={{ padding: "20rem" }}>
+                    <p>There's nothing here!</p>
+                  </div>}/>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 
-const mapStateToProps = (state) => ({
-  isAuthenticated: state.userAuthReducer.isAuthenticated
-});
-
-export default connect(mapStateToProps)(App);
+export default App;
